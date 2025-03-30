c962431467346bf4bb0c8070b8ba3076fb95b9de
eZemenB <buayalewendalk@gmail.com>
2025-03-30 06:04:29 +0300

    Add grant_management/backend/accounts/views.py 1743303869298

---
+++ grant_management/backend/accounts/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer, UserProfileSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
