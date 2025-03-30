1615977d14c1b7dabfc70cfe05bc1cffbcddfc9a
eZemenB <buayalewendalk@gmail.com>
2025-03-30 06:03:45 +0300

    Add grant_management/frontend/src/components/ProtectedRoute.js 1743303825478

---
+++ grant_management/frontend/src/components/ProtectedRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
+
+export default ProtectedRoute;
